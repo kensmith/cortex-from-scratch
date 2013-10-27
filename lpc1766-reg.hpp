2a852461f83229e24bef02d7446f413b2a966b43
Ken Smith <kgsmith@gmail.com>
2013-10-27 10:23:57 -0700

    pll refactor

--- lpc1766-reg.hpp
+++ lpc1766-reg.hpp
@@ -5,10 +5,15 @@
 
 /**
  * The MMIO registers for the LPC1766 that is integrated
- * into the Olimex LPC1766-STK.
+ * into the Olimex LPC1766-STK. Chapter references cite
+ * UM10360 LPC17xx User manual Rev. 2 — 19 August 2010.
  */
 namespace lpc1766
 {
+   /**
+    * 3.7.1 System Controls and Status register (SCS -
+    * 0x400F C1A0)
+    */
    struct scs
    {
       static constexpr unsigned addr = 0x400fc1a0;
@@ -17,6 +22,21 @@ namespace lpc1766
       using oscstat = reg_t<rw_t, addr, 6, 1>;
    };
 
+   struct oscillator
+   {
+      static void enable()
+      {
+         scs::oscen::set();
+         while (!ready())
+         {
+         }
+      }
+      static bool ready() {return scs::oscstat::read() == 1;}
+   };
+
+   /**
+    * Chapter 9: LPC17xx General Purpose Input/Output (GPIO)
+    */
    template <int which_fio>
    struct fio
    {
@@ -32,16 +52,121 @@ namespace lpc1766
       };
    };
 
+   /**
+    * 4.4.1 Clock Source Select register (CLKSRCSEL - 0x400F
+    * C10C)
+    */
+   struct clksrcsel
+   {
+      using clksrc = reg_t<rw_t, 0x400fc10c, 0, 2>;
+      static void use_internal_rc_oscillator() { clksrc::write(0); }
+      static void use_main_oscillator() { clksrc::write(1); }
+      static void use_rtc_oscillator() { clksrc::write(2); }
+   };
+
+   /**
+    * 5.4 Flash Accelerator Configuration register (FLASHCFG
+    * - 0x400F C000)
+    */
+   struct flashcfg
+   {
+      using flashtim = reg_t<rw_t, 0x400fc000, 12, 4>;
+
+      template <int num_clocks>
+      struct use
+      {
+         static_assert(0 <= num_clocks && num_clocks <= 6, "invalid flash timing");
+         static void clocks()
+         {
+            flashtim::write(num_clocks);
+         }
+      };
+   };
+
+   /**
+    * 4.5.2 PLL0 register description
+    */
    template <int which_pll>
    struct pll
    {
       static_assert(0 <= which_pll && which_pll <= 1, "invalid pll");
       static constexpr unsigned base_addr = 0x400fc080+0x20*which_pll;
 
+      /**
+       * 4.5.3 PLL0 Control register (PLL0CON - 0x400F C080)
+       */
       struct con
       {
          using plle = reg_t<rw_t, base_addr, 0, 1>;
          using pllc = reg_t<rw_t, base_addr, 1, 1>;
+
       };
+      static void enable() { con::plle::write(1); feed();}
+      static void disable() { con::plle::write(0); feed();}
+      static void connect() { con::pllc::write(1); feed();}
+      static void disconnect() { con::pllc::write(0); feed();}
+
+      struct cfg
+      {
+         static constexpr unsigned addr = base_addr+4;
+         using msel = reg_t<rw_t, addr, 0, 14>;
+         using nsel = reg_t<rw_t, addr, 16, 8>;
+      };
+
+      static constexpr void M(unsigned m)
+      {
+         cfg::msel::write(m - 1);
+         feed();
+      }
+
+      static constexpr void N(unsigned n)
+      {
+         cfg::nsel::write(n - 1);
+         feed();
+      }
+
+      /**
+       * 4.5.5 PLL0 Status register (PLL0STAT - 0x400F C088)
+       */
+      struct stat
+      {
+         static constexpr unsigned addr = base_addr+8;
+         using msel = reg_t<ro_t, addr, 0, 14>;
+         using nsel = reg_t<ro_t, addr, 16, 8>;
+         using plle = reg_t<ro_t, addr, 24, 1>;
+         using pllc = reg_t<ro_t, addr, 25, 1>;
+         using plock = reg_t<ro_t, addr, 26, 1>;
+      };
+
+      static bool locked()
+      {
+         return stat::plock::read() == 1;
+      }
+
+      /**
+       * 4.5.8 PLL0 Feed register (PLL0FEED - 0x400F C08C)
+       */
+      struct feed
+      {
+         using pllfeed = reg_t<wo_t, base_addr+0xc, 0, 8>;
+
+         ~feed()
+         {
+            pllfeed::write(0xaa);
+            pllfeed::write(0x55);
+         }
+      };
+   };
+
+   /**
+    * 4.7.3 Peripheral Clock Selection registers 0 and 1
+    * (PCLKSEL0 - * 0x400F C1A8 and PCLKSEL1 - 0x400F C1AC)
+    */
+   template <int which>
+   struct pclksel
+   {
+      static_assert(0 <= which && which <= 1, "invalid pclksel");
+      static constexpr unsigned addr = 0x400fc1a8+which*4;
+      using whole = reg_t<rw_t, addr, 0, 32>;
    };
 }
--- lpc1766-stk.hpp
+++ lpc1766-stk.hpp
@@ -5,14 +5,11 @@
 static volatile unsigned * const scs = (volatile unsigned *) 0x400fc1a0;
 static volatile unsigned * const pll0stat = (volatile unsigned *) 0x400fc088;
 static volatile unsigned * const pll0con = (volatile unsigned *) 0x400fc080;
-static volatile unsigned * const pll0feed = (volatile unsigned *) 0x400fc08c;
-static volatile unsigned * const clksrcsel = (volatile unsigned *) 0x400fc10c;
 static volatile unsigned * const pll0cfg = (volatile unsigned *) 0x400fc084;
 static volatile unsigned * const cclkcfg = (volatile unsigned *) 0x400fc104;
 static volatile unsigned * const pclksel0 = (volatile unsigned *) 0x400fc1a8;
 static volatile unsigned * const pclksel1 = (volatile unsigned *) 0x400fc1ac;
 
-static volatile unsigned * const flashcfg = (volatile unsigned *) 0x400fc000;
 static volatile unsigned * const pconp = (volatile unsigned *) 0x400fc0c4;
 
 static volatile unsigned * const fio1dir = (volatile unsigned *) 0x2009c020;
@@ -39,117 +36,39 @@ namespace lpc1766
    {
       struct led_t
       {
-         static void enable()
-         {
-            fio<1>::pin<25>::dir::set();
-         }
-
-         static void on()
-         {
-            fio<1>::pin<25>::clr::set();
-         }
-
-         static void off()
-         {
-            fio<1>::pin<25>::set::set();
-         }
+         static void enable() { fio<1>::pin<25>::dir::set(); }
+         static void on() { fio<1>::pin<25>::clr::set(); }
+         static void off() { fio<1>::pin<25>::set::set(); }
       };
 
       struct oscillator_t
       {
-         static void enable()
-         {
-            scs::oscen::set();
-         }
+         static void enable() { scs::oscen::set(); }
 
-         static bool ready()
-         {
-            return scs::oscstat::read() == 1;
-         }
+         static bool ready() { return scs::oscstat::read() == 1; }
       };
 
       struct pll_t
       {
          static void configure()
          {
-            *flashcfg = (5<<12) | 0x3a;
+            flashcfg::use<5>::clocks();
 
-            stk::oscillator_t::enable();
+            oscillator::enable();
 
-            while (!stk::oscillator_t::ready());
+            pll<0>::disconnect();
+            pll<0>::disable();
+            clksrcsel::use_main_oscillator();
+            pll<0>::M(20);
+            pll<0>::N(1);
 
-            // lpc17xx_um.pdf, 4.5.13 PLL0 setup sequence
-            // 1. Disconnect PLL0 with one feed sequence if PLL0 is already connected.
-            // ks: not necessary after reboot, defaults to disconnected    
-#if 1
-            *pll0con &= ~((unsigned) 1<<1);
-#else
-#endif
-#if 0
-                 ldr	r3, .L5+4
-                 ldr	r2, [r3]
-                 bic	r2, r2, #2
-                 str	r2, [r3]
-         .L5:
-                 .word	1074774016
-                 .word	1074774144
-#endif
+            pclksel<0>::whole::clear();
+            pclksel<1>::whole::clear();
 
-            feed();
-#if 1
-            *pll0con &= ~((unsigned) 1<<0);
-            feed();
+            pll<0>::enable();
 
-            // 3. Change the CPU Clock Divider setting to speed up operation without
-            //    PLL0, if desired.
-
-            // ks: not necessary, we're going to use PLL0
-
-            // 4. Write to the Clock Source Selection Control register to change the
-            //    clock source if needed.
-            
-            *clksrcsel = 1;
-            feed();
-
-            // 5. Write to the PLL0CFG and make it effective with one feed sequence. The
-            //    PLL0CFG can only be updated when PLL0 is disabled.
-            // Fin = 12e6
-            // M = 20
-            // N = 1
-            // Fcco = 480e6
-            // PLL0CFG = M-1 = 20-1 = 19 = 0x13
-
-            *pll0cfg = (20-1);
-            //*pll0cfg = ((25-1)<<0) | ((2-1)<<16);
-            feed();
-
-            // Errata
-            // PCLKSELx.1:Peripheral Clock Selection Registers must
-            // be set before enabling and connecting
-            // PLL0
-            // Introduction:A pair of bits in the Peripheral Clock
-            // Registers (PCLKSEL0 and PCLKSEL1) controls the rate of
-            // the clock signal that will be supplied to APB0 and
-            // APB1 peripherals.
-            // Problem:If the Peripheral Clock Registers (PCLKSEL0
-            // and PCLKSEL1) are set or changed after PLL0 is enabled
-            // and connected, the value written into the Peripheral
-            // Clock Selection Registers may not take effect. It is
-            // not possible to change the Peripheral Clock Selection
-            // settings once PLL0 is enabled and connected.
-            // Workaround:Peripheral Clock Selection Registers must
-            // be set before enabling and connecting PLL0.
-            *pclksel0 = 0;
-            *pclksel1 = 0;
-
-            // 6. Enable PLL0 with one feed sequence.
-
-            *pll0con = 1;
-            feed();
-
-            while (0 == (*pll0stat & (1<<26)))
+            while (!pll<0>::locked())
             {
-               // wait
             }
 
 
@@ -163,7 +82,7 @@ namespace lpc1766
 
             *cclkcfg = 4;
             //*cclkcfg = 3-1;
-            //feed();
+            //pll<0>::feed();
 
             // 8. Wait for PLL0 to achieve lock by monitoring the PLOCK0 bit in the
             //    PLL0STAT register, or using the PLOCK0 interrupt, or wait for a fixed
@@ -185,25 +104,9 @@ namespace lpc1766
             // 9. Connect PLL0 with one feed sequence.
 
             *pll0con |= 1<<1;
-            feed();
+            pll<0>::feed();
 
             //*flashcfg = (4<<12) | 0x3a;
-#endif
-         }
-      private:
-         static void feed()
-         {
-            // TODO refactor PLL configuration using reg_t
-            *pll0feed = 0xaa;
-#if 0
-                 movs	r2, #170
-                 str	r2, [r3, #12]
-#endif
-            *pll0feed = 0x55;
-#if 0
-                 movs	r2, #85
-                 str	r2, [r3, #12]
-#endif
          }
       };
    };
