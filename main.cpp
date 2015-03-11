4454cc0cb148837f2a5b6b04e519281ff7f1c978
Ken Smith <kgsmith@gmail.com>
2015-03-11 08:44:59 -0700

    porting to stellaris

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
-#include "board.hpp"
-
 int main(void)
 {
-   const int start_val = 4800000;
-   const int end_val = 0;
    while (1)
    {
-      for (int i = start_val; i > end_val; --i)
-      {
-         board::led::on();
-         board::uart_t::put_char('1');
-      }
-      for (int i = start_val; i > end_val; --i)
-      {
-         board::led::off();
-         board::uart_t::put_char('0');
-      }
    }
 }
