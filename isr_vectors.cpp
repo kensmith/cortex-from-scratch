2bc373fffe3c404c8282200b44150562fb9122ff
Ken Smith <kgsmith@gmail.com>
2015-03-11 09:20:05 -0700

    porting to stellaris

--- isr_vectors.cpp
+++ isr_vectors.cpp
@@ -3,7 +3,6 @@
 
 [[noreturn]] void reset_handler(void)
 {
-   board::init();
    asm("b main");
    while(1);
 }
@@ -18,29 +17,7 @@
  */
 [[noreturn]] void hard_fault_handler(void)
 {
-   using hfsr = lpc1766::scb::hfsr;
-   using ufsr = lpc1766::scb::ufsr;
-
-   if (hfsr::debugevt::read())
-   {
-      while(1);
-   }
-   else if (hfsr::forced::read())
-   {
-      volatile unsigned ufsr_val = ufsr::whole::read();
-      volatile unsigned bx_bit = lpc1766::id_isar1::bx_instruction_t_bit::read();
-      volatile unsigned blx_bit = lpc1766::id_isar1::blx_instruction::read();
-      while(ufsr::undefinstr::read());
-      while(ufsr_val);
-      while(bx_bit);
-      while(blx_bit);
-      while(1);
-   }
-   else if (hfsr::vecttbl::read())
-   {
-      while(1);
-   }
-   while(1);
+   while (1);
 }
 
 [[noreturn]] void memory_management_fault_handler(void)
@@ -73,177 +50,7 @@
    while (1);
 }
 
-[[noreturn]] void wdt_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void timer0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void timer1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void timer2_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void timer3_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void uart0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void uart1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void uart2_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void uart3_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void pwm1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void i2c0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void i2c1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void i2c2_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void spi_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void ssp0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void ssp1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void pll0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void rtc_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void eint0_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void eint1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void eint2_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void eint3_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void adc_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void bod_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void usb_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void can_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void dma_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void i2s_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void emac_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void rit_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void mc_pwm_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void qei_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void pll1_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void usb_activity_handler(void)
-{
-   while (1);
-}
-
-[[noreturn]] void can_activity_handler(void)
+[[noreturn]] void default_handler(void)
 {
    while (1);
 }
@@ -263,43 +70,39 @@ void (* const isr_sv_call[])() =
    sv_call_handler,
 };
 
-void (* const isr_vector_rest[])() =
+void (* const isr_vector_last_sys[])() =
 {
    pend_sv_handler,
    sys_tick_handler,
-   wdt_handler,
-   timer0_handler,
-   timer1_handler,
-   timer2_handler,
-   timer3_handler,
-   uart0_handler,
-   uart1_handler,
-   uart2_handler,
-   uart3_handler,
-   pwm1_handler,
-   i2c0_handler,
-   i2c1_handler,
-   i2c2_handler,
-   spi_handler,
-   ssp0_handler,
-   ssp1_handler,
-   pll0_handler,
-   rtc_handler,
-   eint0_handler,
-   eint1_handler,
-   eint2_handler,
-   eint3_handler,
-   adc_handler,
-   bod_handler,
-   usb_handler,
-   can_handler,
-   dma_handler,
-   i2s_handler,
-   emac_handler,
-   rit_handler,
-   mc_pwm_handler,
-   qei_handler,
-   pll1_handler,
-   usb_activity_handler,
-   can_activity_handler,
+};
+
+void (* const isr_vector_rest[])() =
+{
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler, default_handler,
+   default_handler, default_handler, default_handler,
 };
--- isr_vectors.hpp
+++ isr_vectors.hpp
@@ -14,46 +14,13 @@ void sv_call_handler(void) __attribute__ ((interrupt ("IRQ")));
 
 void pend_sv_handler(void) __attribute__ ((interrupt ("IRQ")));
 void sys_tick_handler(void) __attribute__ ((interrupt ("IRQ")));
+void default_handler(void) __attribute__ ((interrupt ("IRQ")));
 
-void wdt_handler(void) __attribute__ ((interrupt ("IRQ")));
-void timer0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void timer1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void timer2_handler(void) __attribute__ ((interrupt ("IRQ")));
-void timer3_handler(void) __attribute__ ((interrupt ("IRQ")));
-void uart0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void uart1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void uart2_handler(void) __attribute__ ((interrupt ("IRQ")));
-void uart3_handler(void) __attribute__ ((interrupt ("IRQ")));
-void pwm1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void i2c0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void i2c1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void i2c2_handler(void) __attribute__ ((interrupt ("IRQ")));
-void spi_handler(void) __attribute__ ((interrupt ("IRQ")));
-void ssp0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void ssp1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void pll0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void rtc_handler(void) __attribute__ ((interrupt ("IRQ")));
-void eint0_handler(void) __attribute__ ((interrupt ("IRQ")));
-void eint1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void eint2_handler(void) __attribute__ ((interrupt ("IRQ")));
-void eint3_handler(void) __attribute__ ((interrupt ("IRQ")));
-void adc_handler(void) __attribute__ ((interrupt ("IRQ")));
-void bod_handler(void) __attribute__ ((interrupt ("IRQ")));
-void usb_handler(void) __attribute__ ((interrupt ("IRQ")));
-void can_handler(void) __attribute__ ((interrupt ("IRQ")));
-void dma_handler(void) __attribute__ ((interrupt ("IRQ")));
-void i2s_handler(void) __attribute__ ((interrupt ("IRQ")));
-void emac_handler(void) __attribute__ ((interrupt ("IRQ")));
-void rit_handler(void) __attribute__ ((interrupt ("IRQ")));
-void mc_pwm_handler(void) __attribute__ ((interrupt ("IRQ")));
-void qei_handler(void) __attribute__ ((interrupt ("IRQ")));
-void pll1_handler(void) __attribute__ ((interrupt ("IRQ")));
-void usb_activity_handler(void) __attribute__ ((interrupt ("IRQ")));
-void can_activity_handler(void) __attribute__ ((interrupt ("IRQ")));
 
-__attribute__ ((section(".isr_vector_first_six"))) extern void (* const isr_vectors[])();
-__attribute__ ((section(".isr_sv_call"))) extern void (* const isr_sv_call[])();
-__attribute__ ((section(".isr_vector_rest"))) extern void (* const isr_vector_rest[])();
+__attribute__ ((section(".isr_vector_first_six_section"))) extern void (* const isr_vectors[])();
+__attribute__ ((section(".isr_sv_call_section"))) extern void (* const isr_sv_call[])();
+__attribute__ ((section(".isr_vector_last_sys_section"))) extern void (* const isr_vector_last_sys[])();
+__attribute__ ((section(".isr_vector_rest_section"))) extern void (* const isr_vector_rest[])();
 
 #if defined(__cplusplus)
 }
